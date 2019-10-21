1e4b492afa4a53beaa23562e8174f357ca81c2c7
Anthony Gates <setag9697@yahoo.com>
2019-10-21 18:14:09 -0500

    make it better

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+      name: "Tanner",
+      photo: "https://images.app.goo.gl/YA8Gn6pbuVk6TdtW9",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Devin",
+      photo: "https://images.app.goo.gl/e1srkid1UphQEF487",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Tyler",
+      photo: "https://images.app.goo.gl/eZ9pon9Rb6Uu3ThR7",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Jessy",
+      photo: "https://images.app.goo.gl/xCKa3P6HbsqCyKMu6",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Alexandria",
+      photo: "https://images.app.goo.gl/sQr531HdkhZELrdr8",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Malik",
+      photo: "https://images.app.goo.gl/fiGGS9sCoguaK1QP7",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  module.exports = friends;
